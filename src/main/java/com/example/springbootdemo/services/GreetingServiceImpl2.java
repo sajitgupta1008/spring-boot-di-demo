979184943ded921e09178ee89f2be1aada0a95bb
Sajit Gupta <SajitGupta@rccl.com>
2019-02-22 23:35:16 +0530

    Show profiles usage

--- src/main/java/com/example/springbootdemo/services/GreetingServiceImpl2.java
+++ src/main/java/com/example/springbootdemo/services/GreetingServiceImpl2.java
@@ -1,7 +1,11 @@
 package com.example.springbootdemo.services;
 
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Service;
 
+@Primary
+@Profile("es")
 @Service
 public class GreetingServiceImpl2 implements GreetService {
    
